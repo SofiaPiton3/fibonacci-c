e916703a8b932105622c9a195a3c8d1f02737433
Sofia <sofiaaraujo0320@gmail.com>
2026-08-19 19:35:53 -0300

    Add files via upload

---
+++ fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_fibonacci.c"
+
+int main(int argc, char *argv[]) {
+    int n, resultado;
+    char saida[100];
+
+    if (argc > 1) {
+        n = atoi(argv[1]);
+    } else {
+        printf("Digite um numero inteiro maior ou igual a zero: ");
+        scanf("%d", &n);
+    }
+
+    resultado = fibonacci(n);
+    sprintf(saida, "O fibonacci de %d e %d\n", n, resultado);
+    printf("%s", saida);
+
+    FILE *arquivo = fopen("fibonacci.txt", "w");
+    if (arquivo != NULL) {
+        fprintf(arquivo, "%s", saida);
+        fclose(arquivo);
+    }
+
+    printf("Criado por Sofia Araújo Piton");
+
+    return 0;
+}
---
+++ test_fibonacci.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <assert.h>
+#include "function_fibonacci.c"
+
+int main(void) {
+    assert(fibonacci(0) == 0);
+    assert(fibonacci(1) == 1);
+    assert(fibonacci(6) == 8);
+    assert(fibonacci(9) == 34);
+
+    printf("Todos os testes passaram!\n");
+    printf("Criado por Sofia Araújo Piton");
+
+    return 0;
+}
